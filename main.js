593accb2947cc96f1ec45500eafa83e6cf731acd
JakeRoughton <112968037+JakeRoughton@users.noreply.github.com>
2022-09-19 15:02:10 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log ("Hello World");
+
+console.log ("HH   HH        lll lll                                  lll      dd !!!");
+console.log ("HH   HH   eee  lll lll  oooo   ww      ww  oooo  rr rr  lll      dd !!!");
+console.log ("HHHHHHH ee   e lll lll oo  oo  ww      ww oo  oo rrr  r lll  dddddd !!!");
+console.log ("HH   HH eeeee  lll lll oo  oo   ww ww ww  oo  oo rr     lll dd   dd");
+console.log ("HH   HH  eeeee lll lll  oooo     ww  ww    oooo  rr     lll  dddddd !!!");
+
+                                                                                                                                                                                       
+console.log ("___ _  _ _ ____    _ ____    _ _ _ _ ____ _  _ ____ ___     ____ ____ ____ _    ");
+console.log (" |  |__| | [__     | [__     | | | | |    |_/  |___ |  \    |    |  | |  | |    ");
+console.log (" |  |  | | ___]    | ___]    |_|_| | |___ | \_ |___ |__/    |___ |__| |__| |___ ");
